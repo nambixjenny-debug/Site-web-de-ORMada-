e868f8b621b5bfeb88b0a992b2eb739839ffca7c
Jenny Michael <nambixjenny@gmail.com>
2026-03-15 18:02:23 +0100

    Creation de page histoire du rova et amelioration du design de l'interface visiteur

--- src/Controller/InterfacevisiteurController.php
+++ src/Controller/InterfacevisiteurController.php
@@ -6,13 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+// final class InterfacevisiteurController extends AbstractController
+// {
+//     #[Route('/interfacevisiteur', name: 'app_interfacevisiteur')]
+//     public function index(): Response
+//     {
+//         return $this->render('interfacevisiteur/index.html.twig', [
+//             'controller_name' => 'InterfacevisiteurController',
+//         ]);
+//     }
+
+//     #[Route('/interfacevisiteur/histoire-du-rova', name: 'app_histoire_du_rova')]
+//     public function histoire(): Response
+//     {
+//         return $this->render('interfacevisiteur/histoiredurova.html.twig', [
+//             'controller_name' => 'InterfacevisiteurController',
+//         ]);
+//     }
+// }
 final class InterfacevisiteurController extends AbstractController
 {
     #[Route('/interfacevisiteur', name: 'app_interfacevisiteur')]
     public function index(): Response
     {
-        return $this->render('interfacevisiteur/index.html.twig', [
-            'controller_name' => 'InterfacevisiteurController',
-        ]);
+        return $this->render('interfacevisiteur/index.html.twig');
+    }
+
+    #[Route('/interfacevisiteur/histoiredurova', name: 'app_histoire_du_rova')]
+    public function histoire(): Response
+    {
+        return $this->render('interfacevisiteur/histoiredurova.html.twig');
     }
 }
